eaa52d3ce738bf552f4c13745c461d6387482975
nthuy <thuynguyen.webdev@gmail.com>
2023-09-11 21:43:54 +0700

    linear search

---
+++ AlgorithmsDataStructure/ArraySearching.cs
@@ -0,0 +1,16 @@
+﻿namespace AlgorithmsDataStructure;
+
+public class ArraySearching
+{
+    public static int LinearSearch(int[] arr, int x)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == x)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ AlgorithmsDataStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgorithmsDataStructure;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 2, 3, 4, 10, 40 };
+        int x = 10;
+
+        int result = ArraySearching.LinearSearch(arr, x);
+
+        if (result == -1)
+        {
+            Console.WriteLine("Element is not present in array");
+        }
+        else
+        {
+            Console.WriteLine($"Element is present at index {result}");
+        }
+    }
+}
